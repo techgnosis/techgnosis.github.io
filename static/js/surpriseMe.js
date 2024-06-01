5bed5df4c5f6f58ee1f66658cfea4003193e506e
James Musselwhite <james.musselwhite@gmail.com>
2024-06-01 16:13:11 -0700

    deleting static folder

--- static/js/surpriseMe.js
+++
@@ -1,5 +0,0 @@
-function surpriseMe() {
-    var num_links = document.links.length;
-    var anchor_index = Math.round(Math.random() * num_links)
-    window.location.href = document.links[anchor_index].href
-}
